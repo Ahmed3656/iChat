40eb78b31e10b69d783c77a4af898b57e4e24948
Ahmed Amr Mohamed <ahmedamr4f@gmail.com>
2024-09-06 01:34:01 +0300

    Initial commit:Created project files and established folder structure

---
+++ client/src/pages/GroupChats.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Chats from './Chats';
+import Chat from './Chat';
+
+const GroupChats = () => {
+  return (
+    <div>
+      <Chats />
+      <Chat />
+    </div>
+  )
+}
+
+export default GroupChats
---
+++ server/index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const cors = require('cors');
+const {connect}  = require('mongoose');
+const upload = require('express-fileupload');
+require('dotenv').config();
+
+// Route handlers
+const userRoutes = require('./routes/userRoutes');
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+// Automatically parse data
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// Link the server side with the client side safely
+app.use(cors({credentials: true, origin: "http://localhost:3000"}));
+
+app.use(upload());
+app.use('/uploads', express.static(__dirname + '/uploads'));
+
+// API routes
+// app.use('/api/users', userRoutes);
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the API!');
+});
+
+// Routes error handlers
+const { notFound, errorMiddleware } = require('./middleware/errorMiddleware');
+app.use(notFound);
+app.use(errorMiddleware);
+
+// Database connection
+connect(process.env.MONGO_URI)
+  .then(app.listen(port, () => console.log(`Server running on port ${port}`)))
+  .catch(error => console.log(error));
