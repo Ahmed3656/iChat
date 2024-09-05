0827c9fa011098b877e1ae60e489868b16364e3a
Ahmed Amr Mohamed <ahmedamr4f@gmail.com>
2024-09-06 01:34:01 +0300

    Initial commit:Created project files and established folder structure

---
+++ client/src/index.js
@@ -0,0 +1,51 @@
+import React, { useContext } from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-bootstrap'
+
+import Layout from './components/Layout';
+import ErrorPage from './pages/ErrorPage';
+import DirectChats from './pages/DirectChats';
+import GroupChats from './pages/GroupChats'
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Logout from './pages/Logout';
+import Settings from './pages/Settings';
+
+// User context to pass the loggedin user through the pages for authorization
+import UserProvider, {UserContext} from './context/userContext';
+
+const ConfigureHome = ({ children }) => {
+  const { currUser } = useContext(UserContext);
+  
+  return currUser ? <DirectChats /> : <Login />;
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <UserProvider><Layout /></UserProvider>,
+    errorElement: <ErrorPage />,
+    children: [
+      { index : true, element : (<ConfigureHome />) },
+      { path: "chats", element: <DirectChats /> },
+      { path: "chats/:id", element: <DirectChats /> },
+      { path: "group-chats", element: <GroupChats /> }, 
+      { path: "group-chats/:id", element: <GroupChats /> },
+      { path : "login", element : <Login /> },
+      { path : "register", element : <Register /> },
+      { path : "logout", element : <Logout /> },
+      { path : "settings", element : <Settings /> },
+    ]
+  }
+])
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>
+);
---
+++ client/src/pages/DirectChats.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Chats from './Chats';
+import Chat from './Chat';
+
+const DirectChats = () => {
+  return (
+    <div>
+      <Chats />
+      <Chat />
+    </div>
+  )
+}
+
+export default DirectChats
